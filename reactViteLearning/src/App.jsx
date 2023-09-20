e763ed139e2f7da6f1444441ee99b8922af25880
danielkhayat8 <daniel.khayat8@gmail.com>
2023-09-20 18:38:25 +0300

    Init a new react project with vite Create a simple dog component

---
+++ reactViteLearning/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Dog from './Dog' 
+
+function App() {
+
+  return (
+    
+     <div>
+      <Dog></Dog>
+     </div>
+  )
+}
+
+export default App
---
+++ reactViteLearning/src/Dog.jsx
@@ -0,0 +1,8 @@
+ import "./Dog.css"
+
+ function Dog() {
+
+        return <p className="Dog">Woof Woof Woof</p>
+ }
+
+ export default Dog;
